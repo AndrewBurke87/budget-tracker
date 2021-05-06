d5728ff24cfcd4196ad0f35e5ea2a698920c9974
Andrew Burke <Drewburke39@gmail.com>
2021-05-05 21:52:23 -0600

    added require fs to server.js

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const logger = require("morgan");
 const mongoose = require("mongoose");
 const compression = require("compression");
+const fs = require("fs");
 
 const PORT = process.env.PORT || 3000;
 
